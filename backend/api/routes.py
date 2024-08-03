6f4af1be443b1f1d6fbae386efedc4ae5c24a893
SeanCThomas0 <thomassc@purdue.edu>
2024-08-03 16:36:48 -0500

    remove print

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -11,7 +11,6 @@ import opendp.prelude as dp
 dp.enable_features('contrib')
 laplace_mechanism = dp.space_of(float) >> dp.m.then_laplace(scale=1.)
 dp_value = laplace_mechanism(123.0)
-print(dp_value)
 
 
 
